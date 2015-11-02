a8175a5f23b0afc1ac144201e7b26d920b4b1df8
Chris Mague <github@mague.com>
2015-11-02 14:37:06 -0800

    gen configs

--- manifests/init.pp
+++ manifests/init.pp
@@ -30,12 +30,28 @@
 # Copyright 2015 Your name here, unless otherwise noted.
 #
 class nomadproject (
-  $user = 'root',
-  $group = 'root',
-  $version = '0.1.2',
-  $bin_dir = '/usr/bin',
-  $data_dir = '/opt/nomad',
+  $user             = 'root',
+  $group            = 'root',
+  $version          = '0.1.2',
+  $port             = '4647',
+  $bin_dir          = '/usr/bin',
+  $data_dir         = '/opt/nomad',
+  $nomad_role       = 'client',
+  $bootstrap_expect = 1,
+  $server_list      = [],
+  $config_hash      = {},
   ){
+
+  $config_default = {
+    'data_dir'   => $data_dir,
+    'region'     => 'nomad',
+    'datacenter' => 'nomad',
+    'name'       => $::hostname,
+    'bind_addr'  => $::ipaddress,
+  }
+
+  validate_hash($config_hash)
+  $final_sets = merge($config_default, $config_hash)
 
   $os = downcase($::kernel)
   $download_url = "https://releases.hashicorp.com/nomad/${version}/nomad_${version}_${os}_${::architecture}.zip"
@@ -62,6 +78,30 @@ class nomadproject (
     owner => 'root',
     group => 0,
     mode  => '0555',
+  }
+
+  file { '/etc/nomad.config':
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('nomadproject/nomad.erb'),
+    require => File["${bin_dir}/nomad"],
+    notify  => Service['nomad'],
+  }
+
+  file { '/etc/init/nomad.conf':
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0444',
+    content => template('nomadproject/upstart.erb'),
+  }
+
+  service { 'nomad' :
+    ensure  => running,
+    enable  => true,
+    require => File['/etc/nomad.conf', '/etc/init/nomad.conf'],
   }
 
 }
